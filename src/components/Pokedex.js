b4f0b00ed46bfc3781b81627535a963693819112
jaidernunes <jaidernf@gmail.com>
2022-09-26 11:13:16 -0300

    Req 4: Pass - Basic Requirements Done

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -9,8 +9,10 @@ class Pokedex extends React.Component {
     super(props);
     this.state = {
       pokeIndex: 0,
+      pokeType: '',
     };
     this.nextPokemon = this.nextPokemon.bind(this);
+    this.typeToFilter = this.typeToFilter.bind(this);
   }
 
   nextPokemon(pkmnMax) {
@@ -19,21 +21,67 @@ class Pokedex extends React.Component {
     }));
   }
 
+  typeToFilter(type) {
+    this.setState(() => ({
+      pokeType: type,
+      pokeIndex: 0,
+    }));
+  }
+
   render() {
-    const { pokeIndex } = this.state;
+    const { pokeIndex, pokeType } = this.state;
     const { pokemonList } = this.props;
+    const filteredList = pokemonList.filter((pokemon) => {
+      if (pokeType === '') return true;
+      return pokemon.type === pokeType;
+    });
 
     return (
       <>
         <h1> Pokédex </h1>
         <div className="pokedex">
-          <Pokemon pokemon={ pokemonList[pokeIndex] } />
-          <button
-            type="button"
-            onClick={ () => this.nextPokemon(pokemonList.length) }
-          >
-            Próximo pokémon
-          </button>
+          {/* <Pokemon pokemon={ pokemonList[pokeIndex] } /> */}
+          {/* <Pokemon
+            pokemon={ pokemonList.filter((pokemon) => {
+              if (pokeType === '') return true;
+              return pokemon.type === pokeType;
+            })[pokeIndex] }
+          /> */}
+          <Pokemon
+            pokemon={ filteredList[pokeIndex] }
+          />
+          <div className="panel">
+            <button
+              type="button"
+              className="buttons"
+              onClick={ () => this.nextPokemon(filteredList.length) }
+            >
+              Próximo pokémon
+            </button>
+
+            <button
+              className="buttons"
+              type="button"
+              onClick={ () => this.typeToFilter('Psychic') }
+            >
+              Tipo Psíquico
+            </button>
+            <button
+              className="buttons"
+              type="button"
+              onClick={ () => this.typeToFilter('Fire') }
+            >
+              Tipo Fogo
+            </button>
+            <button
+              className="buttons"
+              type="button"
+              onClick={ () => this.typeToFilter('') }
+            >
+              Limpar Filtro
+            </button>
+
+          </div>
           {/* {pokemonList
             .map((pokemon) => <Pokemon key={ pokemon.id } pokemon={ pokemon } />)} */}
         </div>
